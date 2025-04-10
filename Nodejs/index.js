f185159d2a949a4ce568cbeffd9205f567a2e3ca
Varun Jagtap <jagtapvarun27@gmail.com>
2025-04-10 17:22:27 +0530

    crime route added

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import adminRoutes from "./Routes/adminRoute.js";
+import crimeRoutes from "./Routes/crimeRoute.js";
 
 const app = express();
 
@@ -10,6 +11,8 @@ console.log("ENV DEBUG:", process.env.FIREBASE_PROJECT_ID);
 app.use(express.json());
 
 app.use("/api/admin", adminRoutes);
+app.use("/api/crime", crimeRoutes);
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("Crime Reporting System backend is up and running!");
