3512a220a1fd8be7d8dec382329f1688bde00734
Varun Jagtap <jagtapvarun27@gmail.com>
2025-04-11 08:57:38 +0530

    email service addded

--- Nodejs/Routes/corruptionRoute.js
+++ Nodejs/Routes/corruptionRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { db } from "../firebase.js";
 import multer from "multer";
+import transporter from "../utils/emailService.js";
 
 const router = express.Router();
 
@@ -37,6 +38,29 @@ router.post("/report", upload.single("media"), async (req, res) => {
     };
 
     await db.collection("corruptionReports").add(report);
+
+    const emailBody = `
+      🚨 New Corruption Report 🚨
+
+      Authority Type: ${authorityType}
+      Officer Name: ${officerName}
+      Department: ${department}
+      Location: ${location}
+      Description:
+      ${description}
+
+      Reported By: ${reportedBy || "Anonymous"}
+      Timestamp: ${new Date().toLocaleString()}
+      Media: ${mediaUrl ? `Attached: ${mediaUrl}` : "No media attached"}
+    `;
+
+    await transporter.sendMail({
+      from: process.env.MAIL_USER,
+      to: "vaibhavsatone252@gmail.com",
+      subject: "New Corruption Complaint ",
+      text: emailBody,
+    });
+
     res.status(201).json({ message: "Corruption report filed successfully." });
   } catch (error) {
     res.status(500).json({ error: error.message });
